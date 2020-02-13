167468a4f6407c7665361238a90a338023186c79
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-13 16:38:59 +0900

    create retrofit

--- app/src/main/java/jp/cordea/jetpackcomposearchitecture/appModule.kt
+++ app/src/main/java/jp/cordea/jetpackcomposearchitecture/appModule.kt
@@ -1,6 +1,26 @@
 package jp.cordea.jetpackcomposearchitecture
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+private const val BASE_URL = "https://api.stackexchange.com"
 
 val appModule = module {
+    single {
+        Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BASIC
+                    })
+                    .build()
+            )
+            .build()
+    }
+    single { get<Retrofit>().create(StackExchangeApi::class.java) }
 }
