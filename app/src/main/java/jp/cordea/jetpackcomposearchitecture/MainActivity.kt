20aaaf6fcbc8faf44be09e88c13d3cc2b1c5a1fd
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-13 14:05:05 +0900

    add navigation graph

--- app/src/main/java/jp/cordea/jetpackcomposearchitecture/MainActivity.kt
+++ app/src/main/java/jp/cordea/jetpackcomposearchitecture/MainActivity.kt
@@ -2,58 +2,10 @@ package jp.cordea.jetpackcomposearchitecture
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
-import androidx.ui.core.Text
-import androidx.ui.core.dp
-import androidx.ui.core.setContent
-import androidx.ui.layout.*
-import androidx.ui.material.Button
-import androidx.ui.material.MaterialTheme
-import androidx.ui.material.TopAppBar
-import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            View()
-        }
+        setContentView(R.layout.main_activity)
     }
-}
-
-@Composable
-fun View() {
-    MaterialTheme {
-        Column {
-            TopAppBar(title = { Text(text = "Main") })
-            FlexColumn(
-                modifier = Spacing(top = 40.dp, left = 16.dp, right = 16.dp),
-                crossAxisSize = LayoutSize.Expand,
-                crossAxisAlignment = CrossAxisAlignment.Stretch,
-                mainAxisAlignment = MainAxisAlignment.Center
-            ) {
-                inflexible {
-                    MainButton(text = "a")
-                    MainButton(text = "b")
-                    MainButton(text = "c")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun MainButton(text: String) {
-    Button(
-        text = text,
-        modifier = Spacing(bottom = 16.dp),
-        onClick = {
-        }
-    )
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    View()
 }
