2dd56023eed27bdccf52054585c5f761df37d0e3
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-13 17:30:46 +0900

    update layout

--- app/src/main/java/jp/cordea/jetpackcomposearchitecture/MvvmFragment.kt
+++ app/src/main/java/jp/cordea/jetpackcomposearchitecture/MvvmFragment.kt
@@ -4,17 +4,83 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.Composable
 import androidx.fragment.app.Fragment
+import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.foundation.VerticalScroller
+import androidx.ui.layout.*
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.TopAppBar
+import androidx.ui.text.TextStyle
+import androidx.ui.text.font.FontWeight
+import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+import androidx.ui.unit.sp
 
 class MvvmFragment : Fragment(R.layout.empty_fragment) {
+    private val state = MvvmState()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view = super.onCreateView(inflater, container, savedInstanceState)
-        (view as ViewGroup).setContent { }
+        (view as ViewGroup).setContent { View(state) }
         return view
+    }
+
+    @Composable
+    private fun View(state: MvvmState) {
+        MaterialTheme {
+            Column {
+                TopAppBar(title = { Text(text = "MVVM") })
+                VerticalScroller {
+                    Column {
+                        state.items.forEach { Item(it) }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun Item(model: MvvmListItemModel) {
+        Row(
+            arrangement = Arrangement.Center,
+            modifier = LayoutPadding(16.dp)
+        ) {
+            Text(
+                modifier = LayoutGravity.Center + LayoutPadding(right = 16.dp),
+                text = model.vote
+            )
+            Column {
+                Text(
+                    text = model.title,
+                    style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                )
+                Text(
+                    text = model.name,
+                    style = TextStyle(fontSize = 12.sp),
+                    modifier = LayoutPadding(top = 4.dp)
+                )
+            }
+        }
+    }
+
+
+    @Preview
+    @Composable
+    private fun DefaultPreview() {
+        View(
+            MvvmState(
+                listOf(
+                    MvvmListItemModel("1", "title", "name"),
+                    MvvmListItemModel("2", "title", "name"),
+                    MvvmListItemModel("3", "title", "name")
+                )
+            )
+        )
     }
 }
