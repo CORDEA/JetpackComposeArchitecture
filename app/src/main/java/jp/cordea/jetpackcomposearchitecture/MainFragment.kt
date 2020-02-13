113cb8d13e29c6db387d81808058eb5dd04f027b
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-13 15:09:44 +0900

    update

--- app/src/main/java/jp/cordea/jetpackcomposearchitecture/MainFragment.kt
+++ app/src/main/java/jp/cordea/jetpackcomposearchitecture/MainFragment.kt
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.compose.Composable
 import androidx.fragment.app.Fragment
 import androidx.ui.core.Text
-import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.layout.*
 import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.TopAppBar
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 class MainFragment : Fragment(R.layout.empty_fragment) {
     override fun onCreateView(
@@ -33,7 +33,7 @@ class MainFragment : Fragment(R.layout.empty_fragment) {
                 TopAppBar(title = { Text(text = "Main") })
                 FlexColumn(
                     modifier = Spacing(top = 40.dp, left = 16.dp, right = 16.dp),
-                    crossAxisSize = LayoutSize.Expand,
+                    crossAxisSize = SizeMode.Expand,
                     crossAxisAlignment = CrossAxisAlignment.Stretch,
                     mainAxisAlignment = MainAxisAlignment.Center
                 ) {
